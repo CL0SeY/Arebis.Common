bc9f8c96196b8e933903d26d013d294c5365d60e
Chris Close <christopher.close+gh@gmail.com>
2016-05-21 21:08:43 +1000

    Add in-place copy for testing

---
+++ Arebis.Logging.GrayLog.NetCore/GrayLoggingException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Arebis.Logging.GrayLog.NetCore
+{
+    /// <summary>
+    /// A GrayLog related exception.
+    /// </summary>
+    public class GrayLoggingException : Exception
+    {
+        /// <summary>
+        /// Constructs a default GrayLoggingException.
+        /// </summary>
+        public GrayLoggingException()
+            : this("An exception occurred.")
+        { }
+
+        /// <summary>
+        /// Constructs a GrayLoggingException given a custom message.
+        /// </summary>
+        public GrayLoggingException(string message)
+            : this(message, null)
+        { }
+
+        /// <summary>
+        /// Constructs a GrayLoggingException for an inner exception.
+        /// </summary>
+        public GrayLoggingException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
