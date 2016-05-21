cbd9448ea2d79c287ed40c2cda9e339705b427be
Chris Close <christopher.close+gh@gmail.com>
2016-05-21 21:08:43 +1000

    Add netstandard1.2 (.NET 4.5) and netstandard1.5 (.NET Core 1.0) support as a separate DLL for now.

---
+++ Arebis.Logging.GrayLog.NetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Arebis.Logging.GrayLog.NetCore")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e07749d-8ce9-40dd-be8f-53d7bf328ceb")]
